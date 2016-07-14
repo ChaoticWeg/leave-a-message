bbadf29a31bb6d8be4bdfd98f0d97266204d8300
BuildTools <shawn@chaoticweg.cc>
2016-07-13 21:45:27 -0500

    Add function lookUpPlayerByName

--- src/cc/chaoticweg/mc/LeaveAMessage/LAMPlugin.java
+++ src/cc/chaoticweg/mc/LeaveAMessage/LAMPlugin.java
@@ -1,5 +1,9 @@
 package cc.chaoticweg.mc.LeaveAMessage;
 
+import cc.chaoticweg.mc.LeaveAMessage.command.LAMCommandExecutor;
+import com.google.common.collect.ImmutableList;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -12,9 +16,42 @@ public class LAMPlugin extends JavaPlugin {
     public void onEnable() {
         this.log = this.getLogger();
 
-        // TODO this
+        // TODO
+
+        this.getCommand("msg").setExecutor(LAMCommandExecutor.getInstance(this));
 
         log.info("Enabled.");
+    }
+
+    public OfflinePlayer lookUpPlayerByName(String name) {
+        if (name == null || name.equals("")) {
+            // name is null or empty. this should be checked for before calling this function, so this is Very Bad
+            throw new IllegalArgumentException("Player name may not be null or empty");
+        }
+
+        // TODO is there a better or faster way to do this?
+
+        // check offline players first
+        OfflinePlayer[] offlinePlayers = this.getServer().getOfflinePlayers();
+        for (OfflinePlayer p : offlinePlayers) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                // we have a player offline whose name matches
+                return p;
+            }
+        }
+
+        // check online players second
+        // use ImmutableList to create a snapshot of players immediately, and work with that.
+        // XXX POSSIBLE BUG IN THE FUTURE: player logs in between players snapshot and message send
+        ImmutableList<Player> onlinePlayers = ImmutableList.copyOf(this.getServer().getOnlinePlayers());
+        for (Player p : onlinePlayers) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                // we have a player online whose name matches
+                return p;
+            }
+        }
+
+        return null;
     }
 
 }
