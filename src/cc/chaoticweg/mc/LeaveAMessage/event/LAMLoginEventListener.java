f0d6d0aee42d31c278ace3af11a21550a6f073a3
BuildTools <shawn@chaoticweg.cc>
2016-07-13 22:06:12 -0500

    Add event listener for PlayerLoginEvent

---
+++ src/cc/chaoticweg/mc/LeaveAMessage/event/LAMLoginEventListener.java
@@ -0,0 +1,12 @@
+package cc.chaoticweg.mc.LeaveAMessage.event;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Listens for {@link org.bukkit.event.Event}s.
+ */
+public class LAMLoginEventListener implements Listener {
+
+    
+
+}
